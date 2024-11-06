db6eb8af9fbd61f250a7e50d34c613cc48026c72
MjavadH <m.hacker0110@gmail.com>
2024-11-06 15:06:09 +0330

    Rename project to 'LetterWordGen'

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Code2")]
+[assembly: AssemblyTitle("LetterWordGen")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Code2")]
+[assembly: AssemblyProduct("LetterWordGen")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
